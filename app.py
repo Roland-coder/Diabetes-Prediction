cc4cbafeffa0f3e28b8a16cdc7820dd16ddf7b0b
Roland-coder <n.ngwa@alustudent.com>
2021-04-27 12:02:50 +0200

    another trial

--- app.py
+++ app.py
@@ -11,10 +11,18 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = [x for x in form.values()]
+    data1 = request.form['pregnancies']
+    data2 = request.form['glucose']
+    data3 = request.form['bloodpressure']
+    data4 = request.form['skinthickness']
+    data5 = request.form['insulin']
+    data6 = request.form['bmi']
+    data7 = request.form['diabetespedigreefunction']
+    data8 = request.form['age']
+    data = np.array([data1,data2,data3,data4,data5,data6,data7,data8])
     prediction = model.predict([data])
     output = prediction[0]
-    if(output == 1.0):
+    if (output == 1.0):
         return render_template('index.html', prediction_text='Patient Has Diabetes')
     else:
 
